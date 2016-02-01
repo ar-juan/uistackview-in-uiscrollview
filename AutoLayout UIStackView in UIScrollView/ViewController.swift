bb9dc0cc23eca7701a41292c9f872b68160125e4
Arjan van der Laan <avdlaan85@hotmail.com>
2016-02-01 17:38:43 +0100

    Auto Layout UIStackView in UIScrollView

---
+++ AutoLayout UIStackView in UIScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout UIStackView in UIScrollView
+//
+//  Created by Arjan van der Laan on 01/02/16.
+//  Copyright © 2016 Arjan van der Laan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
